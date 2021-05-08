7771bd8f88dae26156cdb4adea8bed0288f3ae1e
ting1123 <65483413+ting1123@users.noreply.github.com>
2021-05-08 15:27:10 +0800

    Create 66. Plus One.cpp

---
+++ 66. Plus One.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        bool carry = true;
+        for (int i = digits.size()-1; i >= 0 && carry; i--) {
+            digits[i]++;
+            if (digits[i] == 10) {
+                carry = true;
+                digits[i] = 0;
+            }
+            else carry = false;
+        }
+        // digits.resize(digits.size() + 1);
+        if (carry) digits.insert(digits.begin(),1);
+        return digits;
+    }
+};
