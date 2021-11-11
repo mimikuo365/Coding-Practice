62ebcde160879a5160340f917b978f8cf5fa61a7
Mimi Kuan Ting Kuo <65483413+mimikuo365@users.noreply.github.com>
2021-11-11 18:02:28 +0800

    Update 283. Move Zeroes.cpp

--- 283. Move Zeroes.cpp
+++ 283. Move Zeroes.cpp
@@ -1,23 +1,46 @@
+// Challenge Day 4
+// Date: 2021-11-11
+// Topic: Two pointers (Easy)
+// Two methods provided here, with the second one a lot intuitive than the first one.
+
+
+// Method 1 (Mine)
 class Solution {
 public:
-    
     void moveZeroes(vector<int>& nums) {
         int i = 0, j = 0;
-        while (i < nums.size() && j < nums.size()) {
-            if (nums[j] == 0) {
-                // cout << i << " " << j << endl;
-                while (i < nums.size() && (i == j || nums[i] == 0))
-                    i++;
-                // cout << i << " " << j << endl;
-                if (i == nums.size()) return;
-                if (j < i) {
-                    nums[j] = nums[i];
-                    nums[i] = 0;
-                }
-                i++;
+        while (i < nums.size()) {
+            if (nums[i] == 0) {
+                while (j < nums.size() && nums[j] == 0) 
+                    j++;
+                if (j >= nums.size()) 
+                    return;
+                nums[i] = nums[j];
+                nums[j] = 0;
             }
-            else 
-                j++;
+            ++i;
+            ++j;
+        }
+    }
+};
+
+// Method 2 (LeetCode)
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int lastNonZeroFoundAt = 0;
+        // If the current element is not 0, then we need to
+        // append it just in front of last non 0 element we found. 
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                nums[lastNonZeroFoundAt++] = nums[i];
+            }
+        }
+        // After we have finished processing new elements,
+        // all the non-zero elements are already at beginning of array.
+        // We just need to fill remaining array with 0's.
+        for (int i = lastNonZeroFoundAt; i < nums.size(); i++) {
+            nums[i] = 0;
         }
     }
 };
